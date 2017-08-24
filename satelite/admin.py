b123120207fcbb966289e3ff0507ba6f4627402f
Yamid Pico <ypicoleal@users.noreply.github.com>
2017-08-24 00:54:32 -0500

    cambio admin

--- satelite/admin.py
+++ satelite/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from models import Imagen, Basurero, GPS, Reporte
 # Register your models here.
 
-admin.site.register(Imagen)
-admin.site.register(Basurero)
-admin.site.register(GPS)
-admin.site.register(Reporte)
+admin.site.site_header = 'Basureros Satelites'
+
+#admin.site.register(Imagen)
+#admin.site.register(Basurero)
+#admin.site.register(GPS)
+#admin.site.register(Reporte)
