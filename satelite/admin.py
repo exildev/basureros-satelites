d1cc27f618df88765817eca0b93bce3c176b33c0
Yamid Pico <ypicoleal@users.noreply.github.com>
2017-08-24 01:16:27 -0500

    deregistrando grupos

--- satelite/admin.py
+++ satelite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from models import Imagen, Basurero, GPS, Reporte
+from django.contrib.auth.models import Group
 # Register your models here.
 
 admin.site.site_header = 'Basureros Satelites'
+
+admin.site.unregister(Group)
 
 #admin.site.register(Imagen)
 #admin.site.register(Basurero)
