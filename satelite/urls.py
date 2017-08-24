55c8c50015323ffb2c892c6a30ce41cbf7a7bbca
Yamid Pico Leal <ypicoleal@gmail.com>
2017-08-24 15:57:37 -0500

    cambio de url clasificador fixes #9

--- satelite/urls.py
+++ satelite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 import views
 
 urlpatterns = (
-	url(r'^mostrar/$', views.mostrar_basureros, name='mostrar_basureros'),
+	url(r'^clasificar/$', views.mostrar_basureros, name='mostrar_basureros'),
 	url(r'^agregar/$', views.crear_basurero, name='crear_basurero'),
 	url(r'^basureros.json$', views.json_basureros, name='json_basureros'),
 	url(r'^basureros_markers.json$', views.json_basureros_markers, name='json_basureros_markers'),
